c20ecf945b3fdbee34297a0480762e851b2f90eb
zn1k4l3o <leo.znika@tvz.hr>
2023-08-31 22:50:01 +0200

    footer i dio home screena

--- bike-specs/src/app/page.js
+++ bike-specs/src/app/page.js
@@ -1,11 +1,21 @@
-import Image from 'next/image'
-import'./page.module.scss'
+import Link from 'next/link'
+import'./page.scss'
 
 export default function Home() {
   return (
     <main className="home">
       <div className='background-photo'>
-        dsds    
+        <section>
+          <h1>Bike Specs</h1>
+          <h2>Welcome to Bike Specs, a site dedicated to providing information on specifications of bikes from variety of manufacturers.</h2>
+          <Link href="/">
+            <button >View All</button>
+          </Link>
+        </section>
+      </div>
+      <div className='search-home'>
+          <h2>Search bike by name:</h2>
+          
       </div>
     </main>
   )
